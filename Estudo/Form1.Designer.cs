3ce0e7efb55058e6f5dee5d6981f8ecf21e7d02e
Jonatam Luiz Emidio <89087661+Hagarow@users.noreply.github.com>
2021-10-09 12:04:49 -0300

    Add files via upload

---
+++ Estudo/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿
+namespace Estudo
+{
+    partial class frenteCaixa
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.listProd = new System.Windows.Forms.ListBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.tbcodigo = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.tbtotal = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(119, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(109, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Codigo do Produto";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // listProd
+            // 
+            this.listProd.FormattingEnabled = true;
+            this.listProd.ItemHeight = 15;
+            this.listProd.Location = new System.Drawing.Point(389, 40);
+            this.listProd.Name = "listProd";
+            this.listProd.Size = new System.Drawing.Size(172, 214);
+            this.listProd.TabIndex = 1;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Estudo.Properties.Resources.tools_removebg_preview;
+            this.pictureBox1.Location = new System.Drawing.Point(2, 3);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(79, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(153, 82);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(206, 211);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            // 
+            // tbcodigo
+            // 
+            this.tbcodigo.Location = new System.Drawing.Point(234, 40);
+            this.tbcodigo.Name = "tbcodigo";
+            this.tbcodigo.Size = new System.Drawing.Size(125, 23);
+            this.tbcodigo.TabIndex = 4;
+            this.tbcodigo.TextChanged += new System.EventHandler(this.tbcodigo_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(368, 273);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total";
+            // 
+            // tbtotal
+            // 
+            this.tbtotal.Location = new System.Drawing.Point(412, 270);
+            this.tbtotal.Name = "tbtotal";
+            this.tbtotal.Size = new System.Drawing.Size(128, 23);
+            this.tbtotal.TabIndex = 6;
+            // 
+            // frenteCaixa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.ClientSize = new System.Drawing.Size(582, 319);
+            this.Controls.Add(this.tbtotal);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.tbcodigo);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.listProd);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "frenteCaixa";
+            this.Text = "Frente de Caixa";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listProd;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox tbcodigo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox tbtotal;
+    }
+}
+
---
+++ Estudo/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Estudo
+{
+    public partial class frenteCaixa : Form
+    {
+        string[] codProd = new string[6];
+        string[] nomeProd = new string[6];
+        decimal[] valorProd = new decimal[6];
+        decimal somaTotal;
+        public frenteCaixa()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadPoducts();
+            somaTotal = 0;
+        }
+        private void loadPoducts()
+        {
+
+            codProd[1] = "12345";
+            codProd[2] = "12346";
+            codProd[3] = "12347";
+            codProd[4] = "12348";
+            codProd[5] = "12349";
+
+            nomeProd[1] = "Filtro de ar Gol G5";
+            nomeProd[2] = "Filtro de gasolina Gol G5";
+            nomeProd[3] = "Filtro de olho Gol G5";
+            nomeProd[4] = "Jogo de cabo de vela Gol 2009 diant";
+            nomeProd[5] = "Jogo de vela Gol 2009";
+
+            valorProd[1] = 20.61m;
+            valorProd[2] = 25m;
+            valorProd[3] = 29.83m;
+            valorProd[4] = 155.35m;
+            valorProd[5] = 121.23m;
+        }
+
+        private void tbcodigo_TextChanged(object sender, EventArgs e)
+        {
+            if (tbcodigo.Text.Length == 5)
+            {
+                int ind = 0;
+                for (int cp =1; cp < 6; cp++)
+                {
+                    if(tbcodigo.Text == codProd[cp])
+                    {
+                        ind = cp;
+                    }
+                }
+                if(ind > 0)
+                {
+                    listProd.Items.Add(string.Format("#{0} - {1} - {2}",codProd[ind], nomeProd[ind], valorProd[ind]));
+                    somaTotal += valorProd[ind];
+                    tbtotal.Text = Convert.ToString(somaTotal);
+                }
+                else
+                {
+                    MessageBox.Show("Produto não encontrado");
+                }
+            }
+            
+        }
+    }
+}
